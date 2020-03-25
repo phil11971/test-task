44e226c8a9f1068f0bb2465fa7d380d73daa62ac
Филатов Владислав Вячеславович <philatov182@gmail.com>
2020-03-26 00:53:18 +0400

    fix createDB & insert scripts

--- data/script/insert.sql
+++ data/script/insert.sql
@@ -1,18 +1,18 @@
-INSERT INTO T_PATIENT (LASTNAME, FIRSTNAME, PHONE)
-VALUES ('Филатов', 'Владислав', '7999999'),
-       ('Амарян', 'Гурам', '7999999'),
-       ('Щербаков', 'Алексей', '7999999'),
-       ('Сабуров', 'Нурлан', '7999999'),
-       ('Масаев', 'Тамби', '7999999'),
-       ('Рептилоид', 'Рустам', '7999999'),
-       ('Белый', 'Руслан', '7999999');
-INSERT INTO T_DOCTOR (LASTNAME, FIRSTNAME, SPEC)
-VALUES ('Быков', 'Андрей', 'Заведующий терапевтическим отделением больницы и руководитель интернатуры'),
-       ('Быкова', 'Анастасия', 'Главный врач больницы'),
-       ('Купитман', 'Иван', 'Доктор-дерматовенеролог, кандидат медицинских наук, заведующий кожно-венерологическим отделением больницы'),
-       ('Алабаев', 'Тимур', 'Врач-терапевт');
-INSERT INTO T_RECIPE (DOCTORID, PATIENTID, DESCRIPTION, VALIDITY, PRIORITY)
-VALUES (1, 1, 'Корвалол', 45, 'Cito'),
-       (1, 2, 'Мезим', 50, 'Statim'),
-       (2, 1, 'Цитрамон', 412, 'Normal'),
-       (2, 3, 'Эспумизан', 24, 'Cito');
+INSERT INTO PATIENT (LASTNAME, FIRSTNAME, PHONE)
+VALUES ('Filatov', 'Vlad', '8(927)729-86-66'),
+       ('Amaryan', 'Guram', '8(927)729-86-66'),
+       ('Scherbakov', 'Alex', '8(927)729-86-66'),
+       ('Saburov', 'Nurlan', '8(927)729-86-66'),
+       ('Masaev', 'Tambi', '8(927)729-86-66'),
+       ('Reptiloid', 'Rustam', '8(927)729-86-66'),
+       ('Belyi', 'Ruslan', '8(927)729-86-66');
+INSERT INTO DOCTOR (LASTNAME, FIRSTNAME, SPEC)
+VALUES ('Bikov', 'Andrey', 'Head of the therapeutic department of the hospital and head of internship'),
+       ('Bikova', 'Anastasia', 'Head physician of the hospital'),
+       ('Kupitman', 'Ivan', 'Doctor-Dermatovenerologist, Candidate of Medical Sciences, Head of the Department of Skin and Venereology'),
+       ('Alabaev', 'Timur', 'General practitioner');
+INSERT INTO RECIPE (DOCTORID, PATIENTID, DESCRIPTION, VALIDITYDATE, PRIORITY)
+VALUES (1, 1, 'Corvalol', '2018-10-20', 'Cito'),
+       (1, 2, 'Mezim', '2018-10-20', 'Statim'),
+       (2, 1, 'Citramon', '2018-10-20', 'Normal'),
+       (2, 3, 'Espumisan', '2018-10-20', 'Cito');
